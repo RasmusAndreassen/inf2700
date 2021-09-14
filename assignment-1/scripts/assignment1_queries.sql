22538580b5814d93569598cb93e1eab880ab42ba
Rasmus Andreassen <ran081@uit.no>
2021-09-14 21:14:34 +0200

    updated gitignore and queries file (not sure if necessary)

--- assignment-1/scripts/assignment1_queries.sql
+++ assignment-1/scripts/assignment1_queries.sql
@@ -6,10 +6,52 @@
 .mode column
 .header on
 
+--1------------------------------------------------------------------------------------------------------------------------
 
-SELECT C.customerName AS Customer, SUM(OD.quantityOrdered) AS Total
-FROM   Orders O, Customers C, OrderDetails OD
-WHERE  O.customerNumber = C.customerNumber
-    AND O.orderNumber = OD.orderNumber
-GROUP BY O.customerNumber
-ORDER BY Total DESC;
+SELECT  customerName
+FROM    Customers
+WHERE   UPPER(TRIM(country)) IS 'NORWAY';
+
+--2-------------------------------------------------------------------------------------------------------------------------
+
+SELECT productName, productScale
+FROM Products
+WHERE productLine IS 'Classic Cars';
+
+--3-------------------------------------------------------------------------------------------------------------------------
+
+SELECT  O.orderNumber, O.requiredDate, P.productName, OD.quantityOrdered, P.quantityInStock
+FROM    OrderDetails OD, Orders O, Products P
+WHERE   OD.orderNumber = O.orderNumber
+  AND   OD.productCode = P.productCode
+  AND   O.status = 'In Process';
+
+--4-------------------------------------------------------------------------------------------------------------------------
+
+SELECT  C.customerName, C.creditLimit, Pr.totalPrice, Pm.totalPayment, Pr.totalPrice-Pm.totalPayment AS diffPricePayment
+FROM    Customers C NATURAL JOIN
+        ( SELECT  customerNumber, SUM(amount) AS totalPayment
+          FROM    Payments
+          GROUP BY customerNumber) AS Pm,
+        ( SELECT O.customerNumber, SUM(OD.quantityOrdered*OD.priceEach) AS totalPrice
+          FROM Orders O, OrderDetails OD
+          WHERE O.orderNumber = OD.orderNumber
+          GROUP BY O.customerNumber) AS Pr
+WHERE   C.customerNumber = Pr.customerNumber
+  AND   C.customernumber = Pm.customerNumber
+  AND   diffPricePayment > C.creditLimit;
+
+--5-------------------------------------------------------------------------------------------------------------------------
+
+SELECT  customerNumber, customerName
+FROM  (SELECT DISTINCT customerNumber, customerName, productCode
+       FROM   Customers C NATURAL JOIN Orders O NATURAL JOIN OrderDetails OD NATURAL JOIN
+              (SELECT  sOD.productCode
+               FROM    Orders sO NATURAL JOIN OrderDetails sOD
+               WHERE   sO.customerNumber = 219) KL/* key list*/) L --actual list
+GROUP BY  customerNumber
+HAVING  COUNT(*) IN (SELECT COUNT(*)
+        FROM    Orders sO NATURAL JOIN OrderDetails sOD
+        WHERE   sO.customerNumber = 219)
+   AND customerNumber != 219
+ORDER BY customerNumber;
